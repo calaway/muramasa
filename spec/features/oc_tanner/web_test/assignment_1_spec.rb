3308ced96b3f6b98f6df4d0c4ea533f28cb754d3
Calaway <calaway@users.noreply.github.com>
2017-03-09 22:48:59 -0700

    Renamed assignment 1 test for clarity.

--- spec/features/oc_tanner/web_test/assignment_1_spec.rb
+++ spec/features/oc_tanner/web_test/assignment_1_spec.rb
@@ -6,14 +6,14 @@ feature test_name.to_s, sauce: app.sauce do
   scenario test_name.to_s, type: 'acceptance', broken: false,
                            dev: true, qa: true, prod: true do
 
-    expected_desktop_menu_items = {
+    EXPECTED_DESKTOP_MENU_ITEMS = {
       'WHY APPRECIATE' => 'http://www.octanner.com/why-appreciate.html',
       'PRODUCTS' => 'http://www.octanner.com/products.html',
       'WHY CHOOSE US' => 'http://www.octanner.com/why-choose-us.html',
       'INSIGHTS' => 'http://www.octanner.com/insights.html',
       'INSTITUTE' => 'http://www.octanner.com/institute.html',
       'CLIENTS' => 'http://www.octanner.com/clients.html'
-    }
+    }.freeze
 
     app.launch_app(app.url('OCT'))
 
@@ -21,11 +21,11 @@ feature test_name.to_s, sauce: app.sauce do
     expect(current_window.size[0]).to eq 1100
 
     desktop_menu_items = app.oct_landing_page.find_all_desktop_menu_items
-    expect(desktop_menu_items).to eq expected_desktop_menu_items.keys
+    expect(desktop_menu_items).to eq EXPECTED_DESKTOP_MENU_ITEMS.keys
 
     desktop_menu_items.each do |desktop_menu_item|
       page.find('ul#main-nav > li > a', text: desktop_menu_item).click
-      expect(current_url).to eq expected_desktop_menu_items[desktop_menu_item]
+      expect(current_url).to eq EXPECTED_DESKTOP_MENU_ITEMS[desktop_menu_item]
     end
 
     app.close_all_windows
